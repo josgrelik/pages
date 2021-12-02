7d219f18b9f231920845a9860c8d51d292afe581
Wolfgang welcomez <wolfgangwilfredo@gmail.com>
2021-12-02 08:04:56 -0500

    Updates

---
+++ precache-manifest.6e9d840b88603e14fa4023e0a85ba40b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b684619b3ffc8539c5001228c58d773f",
+    "url": "/josgrelik/index.html"
+  },
+  {
+    "revision": "934be1f2c31d32be86bc",
+    "url": "/josgrelik/static/css/2.8850a738.chunk.css"
+  },
+  {
+    "revision": "78c88e18e02a1289c483",
+    "url": "/josgrelik/static/css/main.f7f895e0.chunk.css"
+  },
+  {
+    "revision": "934be1f2c31d32be86bc",
+    "url": "/josgrelik/static/js/2.3092085b.chunk.js"
+  },
+  {
+    "revision": "332bf1b0d236b7fc5455596e6356764c",
+    "url": "/josgrelik/static/js/2.3092085b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c88e18e02a1289c483",
+    "url": "/josgrelik/static/js/main.afd97af4.chunk.js"
+  },
+  {
+    "revision": "06e39a243e8ec5e19a0e",
+    "url": "/josgrelik/static/js/runtime-main.4d610c9e.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/josgrelik/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/josgrelik/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/josgrelik/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/josgrelik/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/josgrelik/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/josgrelik/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "768998c65ce07675ede8536017d4f6ce",
+    "url": "/josgrelik/static/media/image1.768998c6.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "/josgrelik/static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "/josgrelik/static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/josgrelik/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/josgrelik/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/josgrelik/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/josgrelik/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/josgrelik/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/josgrelik/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/josgrelik/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/josgrelik/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/josgrelik/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/josgrelik/static/media/myImage.bd80794f.png"
+  }
+]);
